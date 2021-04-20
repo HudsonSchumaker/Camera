f320b9d906212762239cb775feca2a4ebc9792a5
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-21 00:01:57 +0200

    New classes.

--- Camera/src/br/com/schumaker/camera/terminal/Terminal.java
+++ Camera/src/br/com/schumaker/camera/terminal/Terminal.java
@@ -1,7 +1,9 @@
 package br.com.schumaker.camera.terminal;
 
+import br.com.schumaker.camera.core.MyTask;
+import br.com.schumaker.camera.core.MyThreadFactory;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 /**
  *
@@ -9,12 +11,18 @@ import java.util.concurrent.Executors;
  */
 public class Terminal implements Cmd {
 
-    private ExecutorService threadPool = Executors.newCachedThreadPool();
+    public static int getNumberProcessors() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+    
+    private MyThreadFactory factory = new MyThreadFactory();
+    private ExecutorService   executor = new ThreadPoolFi(getNumberProcessors(), factory);
 
     @Override
     public void executeCommand(String[] commands) {
         for (String command : commands) {
-
+            executor.execute(new MyTask(command));
         }
+        executor.shutdown();
     }
 }
