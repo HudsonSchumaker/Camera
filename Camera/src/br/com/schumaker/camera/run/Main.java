9923e2f4ae98ecf655776a9805d548938d8d8015
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-20 20:40:06 +0200

    New classes.

--- Camera/src/br/com/schumaker/camera/run/Main.java
+++ Camera/src/br/com/schumaker/camera/run/Main.java
@@ -1,5 +1,7 @@
 package br.com.schumaker.camera.run;
 
+import br.com.schumaker.camera.terminal.Terminal;
+
 /**
  *
  * @author hudson
@@ -7,6 +9,12 @@ package br.com.schumaker.camera.run;
 public class Main {
     
     public static void main(String[] args) {
+        var cmd = new Terminal();
         
+        cmd.executeCommand(new String[]{
+            "ffmpeg -i ep3.mp4 video.avi",
+            "ffmpeg -i ep3.mp4 video.flv",
+            "ffmpeg -i ep3.mp4 video.mpeg",
+            "ffmpeg -i ep3.mp4 video.mpg"});
     }
 }
