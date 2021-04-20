871bf2431a5c88d346ebcb8ad46ac1203654cc24
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-20 20:01:19 +0200

    New classes.

--- Camera/src/br/com/schumaker/camera/core/MyTask.java
+++ Camera/src/br/com/schumaker/camera/core/MyTask.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.schumaker.camera.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
- * @author hudso
+ * @author hudson
  */
-public class MyTask {
+public class MyTask implements Runnable {
+
+    private final String command;
+    private Process process = null;
     
+    public MyTask(String command) {
+        this.command = command;
+    }
+
+    @Override
+    public void run() {
+        try {
+            process = Runtime.getRuntime().exec(command);
+            System.err.println(process.isAlive());
+            var in = process.getInputStream();
+            var buffer = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            process.destroy();
+        }
+    }
+    
+    public boolean isTaskRunning() {
+        return process.isAlive();
+    }
 }
