8072735652f826ee19eca7e34030e2431e239474
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-20 19:12:56 +0200

    New classes.

--- Camera/src/br/com/schumaker/camera/terminal/Terminal.java
+++ Camera/src/br/com/schumaker/camera/terminal/Terminal.java
@@ -1,14 +1,43 @@
 package br.com.schumaker.camera.terminal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hudson 
  */
 public class Terminal implements Cmd {
 
+    private List<String> output = new ArrayList<>();
+
     @Override
     public void executeCommand(String[] commands) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        new Thread(() -> {
+            Process process = null;
+            try {
+                for (String command : commands) {
+                    process = Runtime.getRuntime().exec(command);
+                    var in = process.getInputStream();
+                    var buffer = new BufferedReader(new InputStreamReader(in));
+                    String line;
+                    while ((line = buffer.readLine()) != null) {
+                        System.out.println(line);
+                        output.add(line);
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            } finally {
+                process.destroy();
+            }
+        }).start();
     }
     
+    public List<String> getOuput() {
+        return output;
+    }
 }
