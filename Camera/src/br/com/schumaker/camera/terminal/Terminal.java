b93264c6967efe197ddcc38d184e51cd7a753f24
Hudson Schumaker <hudson.schumaker@me.com>
2021-04-20 20:03:33 +0200

    New classes.

--- Camera/src/br/com/schumaker/camera/terminal/Terminal.java
+++ Camera/src/br/com/schumaker/camera/terminal/Terminal.java
@@ -1,42 +1,20 @@
 package br.com.schumaker.camera.terminal;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
- * @author hudson 
+ * @author hudson
  */
 public class Terminal implements Cmd {
 
-    private List<String> output = new ArrayList<>();
+    private ExecutorService threadPool = Executors.newCachedThreadPool();
 
     @Override
     public void executeCommand(String[] commands) {
-        new Thread(() -> {
-            Process process = null;
-            try {
-                for (String command : commands) {
-                    process = Runtime.getRuntime().exec(command);
-                    var in = process.getInputStream();
-                    var buffer = new BufferedReader(new InputStreamReader(in));
-                    String line;
-                    while ((line = buffer.readLine()) != null) {
-                        output.add(line);
-                    }
-                }
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            } finally {
-                process.destroy();
-            }
-        }).start();
-    }
-    
-    public List<String> getOuput() {
-        return output;
+        for (String command : commands) {
+
+        }
     }
 }
